9d6c528c5c267ea38a8e0810951ab9d8f57d15a4
rg-rohit9431 <9431rohitkumar@gmail.com>
2023-10-22 21:17:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const qrText = document.getElementById('qr-text');
+const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+const qrContainer = document.querySelector('.wrapper .right .fullQr');
+const qrContainer2 = document.querySelector('.wrapper .right .smallQr');
+
+generateBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    isEmptyInput();
+});
+
+
+downloadBtn.addEventListener('click', ()=>{
+    let img = document.querySelector('.wrapper .right .fullQr img');
+
+    if(img !== null){
+        let imgAtrr = img.getAttribute('src');
+        downloadBtn.setAttribute("href", imgAtrr);
+    }
+    else{
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`);
+    }
+});
+
+function isEmptyInput(){
+    qrText.value.length > 0 ? generateQRCode() : alert("Enter the text or URL to generate your QR code");;
+}
+function generateQRCode(){
+    qrContainer.innerHTML = "";
+    qrContainer2.innerHTML = "";
+    new QRCode(qrContainer, {
+        text:qrText.value,
+        height:150,
+        width:150,
+        colorLight:"#fff",
+        colorDark:"#000",
+    });
+    new QRCode(qrContainer2, {
+        text:qrText.value,
+        height:100,
+        width:100,
+        colorLight:"#fff",
+        colorDark:"#000",
+    });
+}
